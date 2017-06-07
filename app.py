3be3ff5f6a2ec8fede9a4750910c86f6d8947077
Abhinav Anand <abhinavanand1905@gmail.com>
2017-06-07 13:12:33 +0530

    app

--- app.py
+++ app.py
@@ -52,8 +52,4 @@ def webhook():
 
 
 if __name__ == '__main__':
-    port = int(os.getenv('PORT', 5000))
-
-    print "Starting app on port %d" % port
-
-    app.run(debug=True, port=port, host='0.0.0.0')
+    app.run()
